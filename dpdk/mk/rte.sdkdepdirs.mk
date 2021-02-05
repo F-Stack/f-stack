0803ae9997c864130be8e2be31cc1ccfec711efd
jfb8856606 <fengbojiang@tencent.com>
2021-02-05 09:07:43 +0000

    Remove unnecessary files from DPDK.

--- dpdk/mk/rte.sdkdepdirs.mk
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: BSD-3-Clause
-# Copyright(c) 2010-2014 Intel Corporation
-
-ifeq (,$(wildcard $(RTE_OUTPUT)/.config))
-  $(error "need a make config first")
-endif
-ifeq (,$(wildcard $(RTE_OUTPUT)/Makefile))
-  $(error "need a make config first")
-endif
