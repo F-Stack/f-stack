81e14d4fc9af55a89ec051eb52a7fe6c67313f9e
fengbojiang <fengbojiang@tencent.com>
2025-11-04 12:42:15 +0800

    Add some cleanup action, however, it is incomplete.

--- lib/ff_dpdk_if.c
+++ lib/ff_dpdk_if.c
@@ -2408,6 +2408,10 @@ ff_dpdk_run(loop_func_t loop, void *arg) {
     rte_eal_mp_remote_launch(main_loop, lr, CALL_MAIN);
     rte_eal_mp_wait_lcore();
     rte_free(lr);
+
+    /* FIXME: Cleanup ff_config, freebsd etc. */
+    rte_eal_cleanup();
+    ff_log_close();
 }
 
 void
